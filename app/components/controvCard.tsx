4d5e003fa1070fae11ac542b034396255dc16987
gg-herrera <tg.garciaherrera@gmail.com>
2025-11-25 20:17:30 -0300

    Responsive Update

--- app/components/controvCard.tsx
+++ app/components/controvCard.tsx
@@ -1,27 +1,29 @@
+import "./components.css";
 interface Props {
-    titulo : string;
-    descripcion : string;
+    titulo: string;
+    descripcion: string;
     contexto: string;
-    fuente : string;
+    fuente: string;
     link: string;
     controversia?: boolean;
 }
 
-export default function ControversiaCard({titulo, descripcion, contexto, fuente, controversia, link} : Props){
+export default function ControversiaCard({ titulo, descripcion, contexto, fuente, controversia, link }: Props) {
     return (
-        <div className="bg-orange-200 border-l-4 border-orange-600 rounded-lg p-6 text-center">
-        <h3 className="text-xl font-bold text-slate-900 mb-2">
-            {titulo}
-        </h3>
-        <p className="text-slate-700 leading-relaxed mb-3">
-           {descripcion}
-        </p>
-        <div className="bg-yellow-200 rounded p-3 mb-3">
-            <p className="text-sm text-slate-600">
-                <strong>Contexto:</strong> {contexto}
+        <div className="bg-orange-200 border-l-4 border-orange-600 rounded-lg p-6 text-center" id="controversiaCard">
+            <h3 className="text-xl font-bold text-slate-900 mb-2">
+                {titulo}
+            </h3>
+            <p className="text-slate-700 leading-relaxed mb-3">
+                {descripcion}
             </p>
-        </div>
-        <a href={link} target="blank" className="text-sm text-slate-500 italic">
-            {fuente}
-        </a>
-    </div>)}
+            <div className="bg-yellow-200 rounded p-3 mb-3">
+                <p className="text-sm text-slate-600">
+                    <strong>Contexto:</strong> {contexto}
+                </p>
+            </div>
+            <a href={link} target="blank" className="text-sm text-slate-500 italic">
+                {fuente}
+            </a>
+        </div>)
+}
--- app/components/logrosCard.tsx
+++ app/components/logrosCard.tsx
@@ -1,23 +1,26 @@
+import "./components.css";
+
 interface Props {
-    titulo : string;
-    descripcion : string;
-    fuente : string;
+    titulo: string;
+    descripcion: string;
+    fuente: string;
     logro?: boolean;
     link: string;
 }
 
-const LogrosCard : any = ({titulo, descripcion, fuente, logro, link} : Props) => {
+const LogrosCard: any = ({ titulo, descripcion, fuente, logro, link }: Props) => {
     return (
-         <div className="w-[screen/2] h-[screen/2] bg-green-200 rounded-lg p-6 gap-4 text-center">
+        <div className="w-[screen/2] h-[screen/2] bg-green-200 rounded-lg p-6 gap-4 text-center" id="logrosCard">
             <h3 className="text-xl font-bold text-slate-900 mb-2">
                 {titulo}
             </h3>
             <p className="text-slate-700 leading-relaxed mb-3">
-            {descripcion}
+                {descripcion}
             </p>
             <a href={link} target="blank" className="text-sm text-slate-500 italic">
                 {fuente}
             </a>
-    </div>);}
+        </div>);
+}
 
 export default LogrosCard
